a29f7a6108e81de05deaf0dc55576d39d6255183
Pankaj Shah <51921322+impankaj91@users.noreply.github.com>
2020-10-29 11:04:04 +0530

    Delete loginpage.py

--- Chepter-2-Variables & Data Types/loginpage.py
+++
@@ -1,6 +0,0 @@
-# Author : Pankaj Shah
-# Practical Name : Login Page(Mini Project)
-firstname=input("Enter Your First Name :")
-lastname=input("Enter Your Last Name :")
-phone=int(input("Enter 10 Digit Phonenumber:"))
-pincode=int(input("Enter Pincode :"))
